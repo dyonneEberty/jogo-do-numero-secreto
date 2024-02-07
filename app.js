bc27f19bc8e8f99023cb25c191ccc57ef9118b3f
DyonneCunha <bydyonne@gmail.com>
2024-02-07 16:29:21 -0300

    Alteração no repositorio local

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let listaDeNumerosSorteados = [];
-let numeroLimite = 50;
+let numeroLimite = 20;
 let numeroSecreto = geraNumeroAleatorio();
 let numeroTentativas = 1;
 
@@ -13,7 +13,7 @@ exibirMensagemInicial();
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 50');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 20');
 }
 
 
